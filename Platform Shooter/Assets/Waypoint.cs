da158ece0acc2573993f0648e214ea49fe48dcca
Gaetz <gaetan.blaisecazalet@gmail.com>
2019-01-23 13:29:44 +0100

    Moving platform (with no player convoy) bis

---
+++ Platform Shooter/Assets/Waypoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+	void OnDrawGizmos()
+	{
+	#if UNITY_EDITOR
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(transform.position, 0.05f);
+			Gizmos.DrawLine(transform.position - new Vector3(-0.1f, 0, 0), transform.position - new Vector3(0.1f, 0, 0));
+			Gizmos.DrawLine(transform.position - new Vector3(0, -0.1f, 0), transform.position - new Vector3(0, 0.1f, 0));
+			Gizmos.color = Color.white;
+	#endif
+	}
+
+}
---
+++ Platform Shooter/Assets/_Scripts/MovingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	[SerializeField] float speed;
+	[SerializeField] Transform[] waypoints;
+
+	int currentWp;
+	int NextWp {
+		get {
+			int next = currentWp + 1;
+			return next >= waypoints.Length ? 0 : next;
+		}
+	}
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		Vector2 velocity = (waypoints[currentWp].position - transform.position).normalized * speed * Time.deltaTime;
+		// Reach next node
+		if((transform.position - waypoints[currentWp].position).magnitude <= speed * Time.deltaTime) {
+			velocity = Vector2.ClampMagnitude(velocity, (waypoints[currentWp].position - transform.position).magnitude);
+			currentWp = NextWp;
+		}
+		transform.Translate(velocity);
+	
+	}
+}
