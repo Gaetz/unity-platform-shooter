c8572e9fdf4e340b7d5e302b184e84abc0ab332e
Gaetz <gaetan.blaisecazalet@gmail.com>
2019-01-25 07:28:05 +0100

    Simple projectiles

---
+++ Platform Shooter/Assets/_Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+		float angle;
+		float speed;
+		float lifetime;
+
+		float lifeCounter;
+
+		BoxCollider2D collide;
+		SpriteRenderer spriteRenderer;
+		ParticleSystem particles;
+		Rigidbody2D rbody;
+
+	void Start () {
+		collide = GetComponent<BoxCollider2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		particles = GetComponent<ParticleSystem>();
+		rbody = GetComponent<Rigidbody2D>();
+		particles.Stop();
+	}
+
+	public void Setup(float angle, float speed, float lifetime) {
+		this.angle = angle;
+		this.speed = speed;
+		this.lifetime = lifetime;
+		if(lifetime > 0)
+			Destroy(gameObject, lifetime);
+	}
+
+	void Update() {
+		rbody.velocity = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * speed * Time.deltaTime;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		particles.Play();
+		spriteRenderer.enabled = false;
+		Destroy(gameObject, 0.15f);
+	}
+}
--- Platform Shooter/Assets/_Scripts/WeaponShoot.cs
+++ Platform Shooter/Assets/_Scripts/WeaponShoot.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class WeaponShoot : MonoBehaviour {
 
+	[SerializeField] Projectile projectile;
+  [SerializeField] float cooldown;
+
+	float cooldownCounter;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,11 @@ public class WeaponShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		cooldownCounter += Time.deltaTime;
+		if(Input.GetButton("Fire1") && cooldownCounter > cooldown) {
+			Projectile p = Instantiate(projectile, transform.position, Quaternion.identity);
+			p.Setup(0, 500, 5);
+			cooldownCounter = 0;
+		}
 	}
 }
